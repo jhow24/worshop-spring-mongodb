8e22f12ceebb6e1b25a2dbfb70795c19463690b0
Johnathan Rodrigues <deadjhow@gmail.com>
2022-12-19 10:38:28 -0300

    primeiro commit

---
+++ src/test/java/br/com/erudio/workshopmongo/WorkshomongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshomongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
